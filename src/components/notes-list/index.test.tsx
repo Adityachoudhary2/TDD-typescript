82ada08fc539e6e2473655833580c1bbd229d052
Aditya <choudharyaditya9381@gmail.com>
2025-03-06 15:19:34 +0530

    create code

--- src/components/notes-list/index.test.tsx
+++ src/components/notes-list/index.test.tsx
@@ -1,11 +1,10 @@
 import { render, screen } from "@testing-library/react";
-import { rest } from "msw";
+import { rest, RestRequest, ResponseComposition, RestContext } from "msw";
 import { setupServer } from "msw/node";
-import NotesList from "./index";
-
+import NoteList from "./index";
 
 const server = setupServer(
-    rest.get("/notes-list", (req, res, ctx) => {
+    rest.get("/notes-list", (req: RestRequest, res: ResponseComposition, ctx: RestContext) => {
         return res(ctx.json({
             notes: [
                 {
@@ -18,36 +17,24 @@ const server = setupServer(
                     title: "Learn React",
                     desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, "
                 },
-                
                 {
                     id: 2,
                     title: "Learn React",
-                desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, "
-            },
-               
-            ],
-          })
-        );
-      })
-    );
+                    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, "
+                }
+            ]
+        }));
+    })
+);
 
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
-describe("Given NotesList ", () => {
-    beforeAll(() => {
-   server.listen();
+describe("Given NotesList", () => {
+    test("When notes-list component is mounted THEN render list of notes", async () => {
+        render(<NoteList />);
+        const notesListItems = await screen.findAllByTestId("notes-list-item");
+        expect(notesListItems).toHaveLength(3); // Adjust the expected length based on your mock data
     });
-})
-
-afterEach(() => {
-    server.resetHandlers();
-
 });
-afterAll(() => {
- server.close();
-});
-
-test("When notes-list component is mounted THEN render list of notes", () => {
-    render(<NotesList/>);
-
-    screen.getAllByTestId("notes-list-item");
-});  
--- src/components/notes-list/index.tsx
+++ src/components/notes-list/index.tsx
@@ -1,19 +1,39 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
+type Note = {
+  id: number;
+  title: string;
+  desc: string;
+};
+type notes = Note[];
 
- const NoteList = () => {
-    useEffect(() => {
-        axios.get("notes-list").then((response) => {
-           
-            const {data} = response;
-            const {notes} = data;
-            console.log(notes);
-        })
-    }, [])
+const NoteList = () => {
+  const [notes, setNotes] = useState<notes | null>(null);
+  useEffect(() => {
+    axios.get("notes-list").then((response) => {
+      const { data } = response;
+      const { notes } = data;
+      console.log(notes);
+      setNotes(notes);
+    });
+  }, []);
 
-    return null;
-}; 
-export default NoteList
+  if (notes === null) {
+    return <div>Loading...</div>;
+  }
 
-
+  return (
+    <div>
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id} data-testid="notes-list-item">
+            <h3>{note.title}</h3>
+            <p>{note.desc}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default NoteList;
